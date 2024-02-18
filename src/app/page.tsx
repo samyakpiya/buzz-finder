89113cdd3f396e5fcecee5b51f2d57c7cb7ddbab
Samyak Piya <samyakpiya@gmail.com>
2024-02-17 20:29:13 -0500

    Update page title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </h1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
-        <span className="font-bold italic underline text-[#a4f839]">
+        <span className="font-bold italic underline text-accent">
           10,000 events
         </span>{" "}
         around you.
@@ -16,7 +16,7 @@ export default function Home() {
 
       <form className="w-full sm:w-[580px]">
         <input
-          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"
+          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
           placeholder="Search events in any city..."
           spellCheck={false}
         />
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Container({ children }) {
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export default function Container({ children }: ContainerProps) {
   return (
     <div className="flex flex-col max-w-7xl mx-auto min-h-screen bg-white/[2%]">
       {children}
