f04896696e636e0dc3b159f4db2fe091d5970d2b
Samyak Piya <samyakpiya@gmail.com>
2024-02-17 21:31:18 -0500

    Add remotePatterns for images and implement EventsPage and EventCard components

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,29 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import { BuzzEvent } from "@/lib/types";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const { city } = params;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: BuzzEvent[] = await response.json();
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <H1>Events in Austin</H1>
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <EventsList events={events} />
     </main>
   );
 }
