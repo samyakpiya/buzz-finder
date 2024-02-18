b4377015320fbc4ab8124852a8c011338eb90f14
Samyak Piya <samyakpiya@gmail.com>
2024-02-18 11:01:41 -0500

    Add loading component and event details section

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import H1 from "@/components/h1";
 import Image from "next/image";
+import React from "react";
 
 type EventPageProps = {
   params: {
@@ -51,12 +52,44 @@ export default async function EventPage({ params }: EventPageProps) {
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
 
-            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] focus:scale-105 sm:w-full rounded-md bg-blur border-white/10 border-2 py-2 hover:scale-105 active:scale-[1.02] transition">
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw]sm:w-full rounded-md bg-blur border-white/10 border-2 py-2 state-effects">
               Get tickets
             </button>
           </div>
         </div>
       </section>
+
+      <div className="min-h-[75vh] text-center px-5 py-16">
+        <Section>
+          <SectionHeading>About the event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  );
+}
+
+type SectionProps = {
+  children: React.ReactNode;
+};
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="max-w-4xl mx-auto text-lg leading-8 text-white/75">
+      {children}
+    </p>
   );
 }
